80d23967b73efcbe22d6c3899926b5804dc28f28
dan <md.erhenede@gmail.com>
2024-06-18 16:25:26 +0100

    finished crud operation

--- server/src/Routes/DocumentRoute.ts
+++ server/src/Routes/DocumentRoute.ts
@@ -24,18 +24,14 @@ router.post('/:id', async(req, res) => {
 
 // get user docs
 router.get('/:id', async(req, res) => {
-    const userId = req.params.id
-    const {name,type,serialNumber,connections,billing,status,sharing} = req.body
-
-
     try {
-        const doc = new Document({ userId, name, type, serialNumber, connections, billing, status, sharing });
-        await doc.save();
-        res.status(201).json(doc);
-        
+        const docs = await Document.find({userId: req.params.id})
+        res.status(200).json(docs);
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error);
     }
+    
+    
 })
 
 // update user docs
@@ -45,10 +41,28 @@ router.put('/:id', async(req, res) => {
 
                 if(doc?.userId === req.body.userId){
                     await doc?.updateOne({$set:(req.body)})
-                    res.status(200).json('post updated')
+                    res.status(200).json('document updated')
                 }
                 else(
                     res.status(404).json('can only update user  documents')
+                )
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// delete user docs
+router.delete('/:id', async(req, res) => {
+    try {
+            const doc = await Document.findByIdAndDelete(req.params.id)
+
+                if(doc?.userId === req.body.userId){
+                    await doc?.deleteOne({$set:(req.body)})
+                    res.status(200).json('document deleted')
+                }
+                else(
+                    res.status(404).json('can only delete user  documents')
                 )
         
     } catch (error) {
